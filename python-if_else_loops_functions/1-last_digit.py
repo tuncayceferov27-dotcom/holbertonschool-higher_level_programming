f2277cb2096157b66edd8472f3a4801b709e6410
tuncayceferov27-dotcom <tuncayceferov27@gmail.com>
2026-03-08 18:15:57 +0000

    Fix negative last digit - now matches checker

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 
-# Düzgün son rəqəm (mənfi üçün də işləyir)
+# Düzgün son rəqəm (mənfi üçün də mənfi çıxır)
 last_digit = number % 10
-if number < 0:
-    last_digit = 10-last_digit
+
+print("Last digit of {} is {} and is ".format(number, last_digit), end="")
 
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print("greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print("0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print("less than 6 and not 0")
