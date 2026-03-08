13e8c51408f1ec476aa9eb845e63af4c97369673
tuncayceferov27-dotcom <tuncayceferov27@gmail.com>
2026-03-08 18:53:43 +0000

    fizzbuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,4 @@
-\#!/usr/bin/python3
+#!/usr/bin/env python3
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print("{}".format(i), end=" ")
+            print(i, end=" ")
