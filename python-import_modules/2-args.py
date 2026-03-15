9c24bc2d8cf8cda7979f06eb7063167486712cbc
tuncayceferov27-dotcom <tuncayceferov27@gmail.com>
2026-03-15 15:41:36 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    n = len(sys.argv) - 1
+
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, sys.argv[i]))
