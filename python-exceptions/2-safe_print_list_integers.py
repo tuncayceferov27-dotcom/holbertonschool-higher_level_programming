5ee5137f1cd04e3ec408c4746ff5a0ad751ed56c
tuncayceferov27-dotcom <tuncayceferov27@gmail.com>
2026-03-27 18:06:24 +0000

    Fix safe_print_list_integers logic

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     i = 0
-
-    while i < x:
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except ValueError:
-            pass
-        i += 1
-
-    print()
+    try:
+        while i < x:
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            except (ValueError, TypeError):
+                # Skip non-integers silently
+                pass
+            i += 1
+    except IndexError:
+        # Let IndexError happen when x is too big
+        pass
+    print()  # new line
     return count
