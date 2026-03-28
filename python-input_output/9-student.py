8621eb53dd00c4e5f173cc3e1f5194650614c7dd
tuncayceferov27-dotcom <tuncayceferov27@gmail.com>
2026-03-28 18:55:35 +0000

    Add Student class with to_json method

---
+++ python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module that defines a Student class"""
+
+
+class Student:
+    """Class that defines a student by first_name, last_name, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: The dictionary representation of the student.
+        """
+        return self.__dict__
