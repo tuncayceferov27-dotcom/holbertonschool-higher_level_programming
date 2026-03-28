336d62ebc61d20a309ea2354216419dbb52a620b
tuncayceferov27-dotcom <tuncayceferov27@gmail.com>
2026-03-28 18:43:44 +0000

    Add function to read and print a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a function to read and print a text file"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
